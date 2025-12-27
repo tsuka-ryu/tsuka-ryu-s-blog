61d12c4e72b0b76e267fa8df194eeabc63187122
tsukaryu <drgon8otsuka@gmail.com>
2025-12-28 01:22:52 +0900

    feat: DocsPageに生えてるTOCはコアの機能で、コンポーネント化されてなさそうなので、引っこ抜いてきて追加。不要な実装が紛れてる

---
+++ app/(home)/blog/[slug]/client.tsx
@@ -0,0 +1,216 @@
+"use client";
+
+import {
+  type ComponentProps,
+  createContext,
+  use,
+  useEffect,
+  useEffectEvent,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { cn } from "@/utils/cn";
+// import { useI18n } from '@/contexts/i18n'; TODO: 必要になったら有効にする
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import { useTOCItems } from "@/components/toc";
+import { useActiveAnchor } from "fumadocs-core/toc";
+import { useTreePath } from "fumadocs-ui/contexts/tree";
+import { ChevronDown } from "@/icons";
+// import { LayoutContext } from '../client';
+
+const TocPopoverContext = createContext<{
+  open: boolean;
+  setOpen: (open: boolean) => void;
+} | null>(null);
+
+export function PageTOCPopover({
+  className,
+  children,
+  ...rest
+}: ComponentProps<"div">) {
+  const ref = useRef<HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+  //   const { isNavTransparent } = use(LayoutContext)!;
+
+  const onClick = useEffectEvent((e: Event) => {
+    if (!open) return;
+
+    if (ref.current && !ref.current.contains(e.target as HTMLElement))
+      setOpen(false);
+  });
+
+  useEffect(() => {
+    window.addEventListener("click", onClick);
+
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, []);
+
+  return (
+    <TocPopoverContext
+      value={useMemo(
+        () => ({
+          open,
+          setOpen,
+        }),
+        [setOpen, open]
+      )}
+    >
+      <Collapsible
+        open={open}
+        onOpenChange={setOpen}
+        data-toc-popover=""
+        className={cn(
+          "sticky top-(--fd-docs-row-2) z-10 [grid-area:toc-popover] h-(--fd-toc-popover-height) xl:hidden max-xl:layout:[--fd-toc-popover-height:--spacing(10)]",
+          className
+        )}
+        {...rest}
+      >
+        <header
+          ref={ref}
+          className={cn(
+            "border-b backdrop-blur-sm transition-colors",
+            // (!isNavTransparent || open) && 'bg-fd-background/80',
+            open && "shadow-lg"
+          )}
+        >
+          {children}
+        </header>
+      </Collapsible>
+    </TocPopoverContext>
+  );
+}
+
+export function PageTOCPopoverTrigger({
+  className,
+  ...props
+}: ComponentProps<"button">) {
+  //   const { text } = useI18n();
+  const { open } = use(TocPopoverContext)!;
+  const items = useTOCItems();
+  const active = useActiveAnchor();
+  const selected = useMemo(
+    () => items.findIndex((item) => active === item.url.slice(1)),
+    [items, active]
+  );
+  const path = useTreePath().at(-1);
+  const showItem = selected !== -1 && !open;
+
+  return (
+    <CollapsibleTrigger
+      className={cn(
+        "flex w-full h-10 items-center text-sm text-fd-muted-foreground gap-2.5 px-4 py-2.5 text-start focus-visible:outline-none [&_svg]:size-4 md:px-6",
+        className
+      )}
+      data-toc-popover-trigger=""
+      {...props}
+    >
+      <ProgressCircle
+        value={(selected + 1) / Math.max(1, items.length)}
+        max={1}
+        className={cn("shrink-0", open && "text-fd-primary")}
+      />
+      <span className="grid flex-1 *:my-auto *:row-start-1 *:col-start-1">
+        <span
+          className={cn(
+            "truncate transition-all",
+            open && "text-fd-foreground",
+            showItem && "opacity-0 -translate-y-full pointer-events-none"
+          )}
+        >
+          {path?.name ?? "TODO: text.toc"}
+        </span>
+        <span
+          className={cn(
+            "truncate transition-all",
+            !showItem && "opacity-0 translate-y-full pointer-events-none"
+          )}
+        >
+          {items[selected]?.title}
+        </span>
+      </span>
+      <ChevronDown
+        className={cn(
+          "shrink-0 transition-transform mx-0.5",
+          open && "rotate-180"
+        )}
+      />
+    </CollapsibleTrigger>
+  );
+}
+
+export function PageTOCPopoverContent(props: ComponentProps<"div">) {
+  return (
+    <CollapsibleContent
+      data-toc-popover-content=""
+      {...props}
+      className={cn("flex flex-col px-4 max-h-[50vh] md:px-6", props.className)}
+    >
+      {props.children}
+    </CollapsibleContent>
+  );
+}
+
+interface ProgressCircleProps
+  extends Omit<React.ComponentProps<"svg">, "strokeWidth"> {
+  value: number;
+  strokeWidth?: number;
+  size?: number;
+  min?: number;
+  max?: number;
+}
+
+function clamp(input: number, min: number, max: number): number {
+  if (input < min) return min;
+  if (input > max) return max;
+  return input;
+}
+
+function ProgressCircle({
+  value,
+  strokeWidth = 2,
+  size = 24,
+  min = 0,
+  max = 100,
+  ...restSvgProps
+}: ProgressCircleProps) {
+  const normalizedValue = clamp(value, min, max);
+  const radius = (size - strokeWidth) / 2;
+  const circumference = 2 * Math.PI * radius;
+  const progress = (normalizedValue / max) * circumference;
+  const circleProps = {
+    cx: size / 2,
+    cy: size / 2,
+    r: radius,
+    fill: "none",
+    strokeWidth,
+  };
+
+  return (
+    <svg
+      role="progressbar"
+      viewBox={`0 0 ${size} ${size}`}
+      aria-valuenow={normalizedValue}
+      aria-valuemin={min}
+      aria-valuemax={max}
+      {...restSvgProps}
+    >
+      <circle {...circleProps} className="stroke-current/25" />
+      <circle
+        {...circleProps}
+        stroke="currentColor"
+        strokeDasharray={circumference}
+        strokeDashoffset={circumference - progress}
+        strokeLinecap="round"
+        transform={`rotate(-90 ${size / 2} ${size / 2})`}
+        className="transition-all"
+      />
+    </svg>
+  );
+}
---
+++ app/(home)/blog/[slug]/page.tsx
@@ -0,0 +1,122 @@
+import { blog } from "@/lib/source";
+import { getMDXComponents } from "@/mdx-components";
+import { notFound } from "next/navigation";
+import path from "node:path";
+import { ReactNode } from "react";
+import {
+  PageTOCPopover,
+  PageTOCPopoverContent,
+  PageTOCPopoverTrigger,
+} from "./client";
+import * as TocDefault from "@/components/toc/default";
+import * as TocClerk from "@/components/toc/clerk";
+import { TOCProvider, TOCScrollArea } from "@/components/toc";
+
+export function generateStaticParams(): { slug: string }[] {
+  return blog.getPages().map((page) => ({
+    slug: page.slugs[0],
+  }));
+}
+
+export default async function Page(props: PageProps<"/blog/[slug]">) {
+  const params = await props.params;
+  const page = blog.getPage([params.slug]);
+  // TODO: 型をちゃんとしたほうがいい
+  const tocPopoverOptions = {
+    style: "clerk",
+    header: undefined,
+    footer: undefined,
+  };
+  const tocOptions = {
+    style: "clerk",
+    header: undefined,
+    footer: undefined,
+    single: true,
+  };
+
+  if (!page) notFound();
+  const { body: Mdx, toc } = await page.data.load();
+
+  const wrapper = (children: ReactNode) => (
+    <TOCProvider single={tocOptions.single} toc={toc}>
+      {children}
+    </TOCProvider>
+  );
+
+  return wrapper(
+    <div
+      className="grid grid-cols-1 xl:grid-cols-[1fr_268px] [--fd-docs-height:100dvh] [--fd-toc-width:268px]"
+      style={
+        {
+          "--fd-docs-row-1": "var(--fd-banner-height, 0px)",
+          gridTemplate: `"toc-popover toc-popover"
+                         "main toc" 1fr`,
+        } as React.CSSProperties
+      }
+    >
+      <PageTOCPopover>
+        <PageTOCPopoverTrigger />
+        <PageTOCPopoverContent>
+          {tocPopoverOptions.header}
+          <TOCScrollArea>
+            {tocPopoverOptions.style === "clerk" ? (
+              <TocClerk.TOCItems />
+            ) : (
+              <TocDefault.TOCItems />
+            )}
+          </TOCScrollArea>
+          {tocPopoverOptions.footer}
+        </PageTOCPopoverContent>
+      </PageTOCPopover>
+
+      <article className="flex flex-col mx-auto w-full max-w-200 px-4 py-8 [grid-area:main]">
+        <div className="flex flex-row gap-4 text-sm mb-8">
+          <div>
+            <p className="mb-1 text-fd-muted-foreground">Written by</p>
+            <p className="font-medium">{page.data.author}</p>
+          </div>
+          <div>
+            <p className="mb-1 text-sm text-fd-muted-foreground">At</p>
+            <p className="font-medium">
+              {new Date(
+                page.data.date ??
+                  path.basename(page.path, path.extname(page.path))
+              ).toDateString()}
+            </p>
+          </div>
+        </div>
+
+        <h1 className="text-3xl font-semibold mb-4">{page.data.title}</h1>
+        <p className="text-fd-muted-foreground mb-8">{page.data.description}</p>
+
+        <div className="prose min-w-0 flex-1">
+          <Mdx components={getMDXComponents()} />
+        </div>
+      </article>
+
+      <div
+        id="nd-toc"
+        className="sticky top-[calc(var(--fd-docs-row-1)+1rem)] h-[calc(var(--fd-docs-height)-var(--fd-docs-row-1)-1rem)] flex flex-col [grid-area:toc] w-(--fd-toc-width) pt-12 pe-4 pb-2 max-xl:hidden"
+      >
+        {tocOptions.header}
+        <h3
+          id="toc-title"
+          className="inline-flex items-center gap-1.5 text-sm text-fd-muted-foreground"
+        >
+          {/* TODO: 有効化したい */}
+          {/* <Texx className="size-4" /> */}
+          {/* <I18nLabel label="toc" /> */}
+        </h3>
+        {/* TODO: スクロール位置の判定が微妙なので、一番したまでいくように修正したい */}
+        <TOCScrollArea>
+          {tocOptions.style === "clerk" ? (
+            <TocClerk.TOCItems />
+          ) : (
+            <TocDefault.TOCItems />
+          )}
+        </TOCScrollArea>
+        {tocOptions.footer}
+      </div>
+    </div>
+  );
+}
